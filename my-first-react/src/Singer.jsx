187761e0b317eb1da9fd046c00b7bf878bcd825c
Ratulhasanuday <ratulhasanudoy412220@gmail.com>
2024-03-31 22:06:48 +0600

    Project add

--- my-first-react/src/Singer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Singer({singer}) {
-    console.log(singer);
-    return (
-        <div>
-            <h3>Singer: {singer.name}</h3>
-            <p>Age: {singer.age}</p>
-        </div>
-    )
-}
